de60b3c27e06bba42f5ead4b25dfdb64fcbc32be
Brenda <brenditech@gmail.com>
2021-05-08 21:37:34 -0300

    starting

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { styles } from '../theme/appTheme';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
+
+export const HomeScreen = () => {
+
+    const {top} = useSafeAreaInsets();
+    usePokemonPaginated();
+    return (
+        <>
+            <Image
+                source={ require('../assets/pokebola.png')}
+                style={styles.pokebolaBG}
+            />
+            <Text style={{ 
+                ...styles.title,
+                ...styles.globalMargin,
+                top: top + 20
+            }}>
+                Pokedex
+            </Text>
+        </>
+    )
+}
